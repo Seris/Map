6b061afee6571b7e17d5a6d0b1dc8a1b32c9cc32
Thalya <seris@protonmail.ch>
2016-10-23 18:52:06 +0000

    fix list element deletion

--- disco/list.c
+++ disco/list.c
@@ -7,6 +7,8 @@
 #include <types/disco_intern.h>
 #include <types/disco.h>
 
+void liste_free(music_t* music);
+
 Discotheque liste_creer_discotheque(void) {
     return mlist_empty();
 }
@@ -17,12 +19,7 @@ void liste_detruire_discotheque(Discotheque d){
     mlist_t* disco = d;
 
     while((music = mlist_rem(&disco, NULL)) != NULL){
-        free(music->title);
-        free(music->author);
-        free(music->label);
-        free(music->date);
-        free(music->style);
-        free(music);
+        liste_free(music);
     }
 }
 
@@ -43,17 +40,20 @@ Discotheque liste_inserer(Discotheque di, Titre t, Interprete i, Label l, Date d
 
 
 Discotheque liste_rechercher(Discotheque d, Titre t){
-    Discotheque result = NULL;
+    Discotheque result = liste_creer_discotheque();
     
+    music_t* music;
     mlist_t* disco = d;
     while(result == NULL && disco != NULL){
         if(strcmp(disco->key, t) == 0){
-            result = disco->value;
+            music = disco->value;
+            result = liste_inserer(result,
+                music->title, music->author, music->label, music->date, music->style);
         }
         disco = disco->next;
     }
 
-    return d;
+    return result;
 }
 
 
@@ -64,26 +64,22 @@ Discotheque liste_supprimer(Discotheque d, Titre t){
 
     while(child != NULL && music == NULL){
         if(strcmp(child->key, t) == 0){
-            music = mlist_rem(&child, NULL);
             if(parent != NULL){
-                parent->next = child;
+                parent->next = child->next;
+            } else {
+                d = parent;
             }
+
+            music = mlist_rem(&child, NULL);
+            liste_free(music);
         } else {
             parent = child;
             child = child->next;
         }
     }
 
-    if(music != NULL){
-        free(music->title);
-        free(music->author);
-        free(music->label);
-        free(music->date);
-        free(music->style);
-        free(music);
-    }
 
-    return parent;
+    return d;
 }
 
 int liste_compter_interpretes(Discotheque d){
@@ -118,5 +114,16 @@ void liste_afficher(Discotheque d){
 void liste_afficher_album(Discotheque d){
     if(d != NULL){
         
+    }
+}
+
+void liste_free(music_t* music){
+    if(music != NULL){
+        free(music->title);
+        free(music->author);
+        free(music->label);
+        free(music->date);
+        free(music->style);
+        free(music);
     }
 }
